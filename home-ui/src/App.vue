79196d81cc99b621d47e2ab45e33a404649d327c
邱 <1766226354@qq.com>
2021-01-14 23:25:00 +0800

    把前端项目合并到一个目录下，方便管理

---
+++ home-ui/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view />
+   <foot></foot>
+  </div>
+</template>
+<script>
+import Foot from '@components/layout/foot';
+export default {
+  name: 'app',
+  components: {
+    Foot
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ home-ui/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>项目申明文字</h1>
+  </div>
+</template>
